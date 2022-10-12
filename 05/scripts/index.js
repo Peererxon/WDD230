411e127cd0db26e3b2a5883c271895b7da640126
anderson gil <andersongil.dev@gmail.com>
2022-10-11 21:46:21 -0400

    Asssigments 5

---
+++ 05/scripts/index.js
@@ -0,0 +1,57 @@
+const submitButton = document.getElementById("submitCharadter");
+let charapters = [];
+submitButton.addEventListener("click", () => {
+  const inputElement = document.getElementById("favchap");
+  if (inputElement.value.trim().length > 0) {
+    charapters.unshift(inputElement.value);
+    addELement(inputElement.value);
+  } else {
+    showWarning();
+    submitButton.setAttribute("disabled", true);
+  }
+});
+
+document.getElementById("nose").addEventListener("click", removeElement);
+
+function showWarning() {
+  const warning = document.createElement("div");
+
+  warning.textContent = "Not empy elements allowed";
+  warning.id = "alert";
+  warning.className = "alert";
+  document.body.appendChild(warning);
+
+  setTimeout(() => {
+    submitButton.removeAttribute("disabled");
+    document.body.removeChild(warning);
+  }, 1000);
+}
+
+function addELement(charapter) {
+  const list = document.getElementsByClassName("list")[0];
+  const li = document.createElement("li");
+  const button = document.createElement("button");
+  const icon = document.createElement("i");
+
+  icon.className = "remove";
+  button.ariaLabel = "remove element";
+  li.textContent = charapter;
+
+  button.appendChild(icon);
+  button.addEventListener("click", removeElement);
+  li.appendChild(button);
+  list.appendChild(li);
+}
+
+function removeElement(e) {
+  const list = document.getElementsByClassName("list")[0];
+  const selectedElement = e.target.parentElement.parentElement;
+  const liValue = selectedElement.textContent.trim();
+  charapters = charapters.filter((e) => e !== liValue);
+  // this code is because sometimes we could press the X or the button and technically they are at differents levels od the DOM tree
+  try {
+    list.removeChild(selectedElement);
+  } catch {
+    list.removeChild(e.target.parentElement);
+  }
+}
