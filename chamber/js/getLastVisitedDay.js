eec73834ea759d52fe3909ec8a97b2578e5982fb
anderson gil <andersongil.dev@gmail.com>
2022-10-26 16:39:35 -0400

    I implemented a js that display last visit day

---
+++ chamber/js/getLastVisitedDay.js
@@ -0,0 +1,39 @@
+const getLastVisitedDay = () =>
+  localStorage.getItem("lastDayVisitedDiscoverPage");
+
+const setLastDayVisited = () => {
+  const currentDate = new Date();
+
+  localStorage.setItem("lastDayVisitedDiscoverPage", currentDate);
+};
+
+const getDifferenceInDays = (lastVisitDate) => {
+  const lastVisitDate2 = new Date(lastVisitDate);
+  const currentDate = new Date();
+
+  // To calculate the time difference of two dates
+  const differenceInTime = currentDate.getTime() - lastVisitDate2.getTime();
+
+  // To calculate the no. of days between two dates
+  const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+
+  return Math.floor(differenceInDays);
+};
+
+const printLastVisitedDay = () => {
+  const bannerMessage = document.querySelector("span#bannerMessage");
+
+  const lastDayVisitedDiscoverPage = getLastVisitedDay();
+
+  if (!lastDayVisitedDiscoverPage) {
+    bannerMessage.textContent = "0 days from your last visit";
+    setLastDayVisited();
+    return;
+  }
+
+  const differenceInDays = getDifferenceInDays(lastDayVisitedDiscoverPage);
+
+  bannerMessage.textContent = `${differenceInDays} days from your last visit`;
+};
+
+printLastVisitedDay();
