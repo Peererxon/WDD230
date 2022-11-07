dbc065be17280c9a2a054365fb9bdf109db87a52
anderson gil <andersongil.dev@gmail.com>
2022-11-07 10:51:34 -0400

    WIP: mobile responsive

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -16,10 +16,16 @@ function displayProphets(prophet) {
   // Create elements to add to the document
   let card = document.createElement("section");
   let h2 = document.createElement("h2");
+  let dateOfBirthPTag = document.createElement("p");
+  let placeOfBirthPTag = document.createElement("p");
   let portrait = document.createElement("img");
 
   // Change the textContent property of the h2 element to contain the prophet's full name
-  h2.textContent = prophet.name + " " + prophet.lastname;
+  h2.textContent = `${prophet.name} ${prophet.lastname}`;
+
+  dateOfBirthPTag.textContent = `Date of birth: ${prophet.birthdate}`;
+
+  placeOfBirthPTag.textContent = `Place of birth: ${prophet.birthplace}`;
 
   // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   portrait.setAttribute("src", prophet.imageurl);
@@ -31,6 +37,8 @@ function displayProphets(prophet) {
 
   // Add/append the section(card) with the h2 element
   card.appendChild(h2);
+  card.appendChild(dateOfBirthPTag);
+  card.appendChild(placeOfBirthPTag);
   card.appendChild(portrait);
 
   // Add/append the existing HTML div with the cards class with the section(card)
