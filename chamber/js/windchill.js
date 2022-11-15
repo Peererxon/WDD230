2ee33481cbb0b8fe62c5c507c68ea58d8da5120d
anderson gil <andersongil.dev@gmail.com>
2022-11-15 12:34:13 -0400

    end weather fetch

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,8 +1,5 @@
 const windchill = (temp, wind) => {
-  const windchill = document.querySelector("#windchill");
   const windDegree = document.querySelector("#windDegree");
-
-  windchill.textContent = "N/A";
 
   if (temp <= 50 && wind >= 3) {
     let chill = Math.round(
@@ -11,11 +8,54 @@ const windchill = (temp, wind) => {
         35.75 * Math.pow(wind, 0.16) +
         0.4275 * temp * Math.pow(wind, 0.16)
     );
-    windchill.textContent = chill.toFixed(0);
     windDegree.innerHTML = "&#8457;";
+    return chill.toFixed(0);
+  } else {
+    return "N/A";
   }
 };
 
 const temperature = parseFloat(document.querySelector("#temp").textContent);
 const windSpeed = parseFloat(document.querySelector("#wind").textContent);
 windchill(temperature, windSpeed);
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Barquisimeto&units=imperial&appid=9292920acb5554113149ccdebbf09d8b`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+apiFetch();
+
+function displayResults(data) {
+  // Select elements
+  const temp = document.querySelector("#temp");
+  const wind = document.querySelector("#wind");
+  const weatherIcon = document.querySelector("#weathericon");
+  const figcaption = document.querySelector("#figcaption");
+  const windChill = document.querySelector("#windchill");
+
+  const temperature = data.main.temp;
+  const iconSrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  const desc = data.weather[0].description;
+  const windSpeed = data.wind.speed;
+
+  // Set values
+  temp.textContent = temperature;
+  wind.textContent = windSpeed;
+  weatherIcon.setAttribute("src", iconSrc);
+  weatherIcon.setAttribute("alt", desc);
+  figcaption.textContent = desc;
+  windChill.textContent = windchill(temperature, windSpeed);
+}
