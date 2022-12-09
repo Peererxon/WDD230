834de772a4448b96cca96aa45a8e0c354e5ec6cc
anderson gil <andersongil.dev@gmail.com>
2022-12-09 12:01:25 -0400

    WIP: display form result

--- bountilsfoods/js/handleForm.js
+++ bountilsfoods/js/handleForm.js
@@ -2,6 +2,36 @@ import openModal from "./handleModal.js";
 
 const form = document.querySelector("#fresh-form");
 
+const setModalContent = ({
+  firstName,
+  email,
+  cellPhoneNumber,
+  ingredient1,
+  ingredient2,
+  ingredient3,
+  specialInstructions,
+}) => {
+  // Get inputs value
+  document.querySelector("#firstNameModal").textContent = firstName;
+
+  document.querySelector("#emailModal").textContent = email;
+
+  document.querySelector("#cellPhoneNumberModal").textContent = cellPhoneNumber;
+
+  document.querySelector("#ingredient1Modal").textContent = ingredient1;
+
+  document.querySelector("#ingredient2Modal").textContent = ingredient2;
+
+  document.querySelector("#ingredient3Modal").textContent = ingredient3;
+
+  document.querySelector("#specialInstructionsModal").textContent =
+    specialInstructions;
+
+  document.querySelector("#orderDateModal").textContent =
+    new Date().toDateString();
+
+  openModal();
+};
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -22,5 +52,13 @@ form.addEventListener("submit", (e) => {
     "#specialInstructions"
   ).value;
 
-  openModal();
+  setModalContent({
+    firstName,
+    email,
+    cellPhoneNumber,
+    ingredient1,
+    ingredient2,
+    ingredient3,
+    specialInstructions,
+  });
 });
