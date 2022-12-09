ba2437800e1cab3b0f1a09a5a2d0d6b8fe79f85d
anderson gil <andersongil.dev@gmail.com>
2022-12-09 11:25:02 -0400

    get values from form

--- bountilsfoods/js/handleForm.js
+++ bountilsfoods/js/handleForm.js
@@ -2,5 +2,21 @@ const form = document.querySelector("#fresh-form");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  alert("subiendo");
+
+  // Get inputs value
+  const firstName = document.querySelector("#firstName").value;
+
+  const email = document.querySelector("#email").value;
+
+  const cellPhoneNumber = document.querySelector("#cellPhoneNumber").value;
+
+  const ingredient1 = document.querySelector("#ingredient1").value;
+
+  const ingredient2 = document.querySelector("#ingredient2").value;
+
+  const ingredient3 = document.querySelector("#ingredient3").value;
+
+  const specialInstructions = document.querySelector(
+    "#specialInstructions"
+  ).value;
 });
